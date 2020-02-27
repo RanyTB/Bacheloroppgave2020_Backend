9f2c70b8f701b21ee4709f846bfb51b6af816aa6
Rany Tarek Bouorm <rany-94@hotmail.com>
2020-02-27 14:00:45 +0100

    remove request logging in test env

--- middleware/requestLogger.js
+++ middleware/requestLogger.js
@@ -1,4 +1,5 @@
 module.exports = function requestLogger(req, res, next) {
+  if (process.env.NODE_ENV === "test") return next();
   console.log(`Request: ${req.originalUrl}`, JSON.stringify(req.body));
   next();
 };
