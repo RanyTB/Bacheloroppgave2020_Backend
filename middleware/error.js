cafbf5df1e195d5ae6e230a28d669a2aeb172135
Markus12321 <55793110+Markus12321@users.noreply.github.com>
2020-03-11 15:51:47 +0100

    add console log of err object

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,5 @@
 module.exports = function(err, req, res, next) {
   //Consider using winston to log error object
-  if (err) console.log("something failed +\n" + err);
+  if (err) console.log("Something failed \n", err);
   res.status(500).send("Something failed");
 };
