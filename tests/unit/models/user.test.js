4cbc78b3adf84ba77f263c38ddf6333c1d83db24
Markus Hellestveit <markus.bikilla.hellestveit@gmail.com>
2020-03-12 13:13:21 +0100

    add test for valid admin and non admin jwt

--- tests/unit/models/user.test.js
+++ tests/unit/models/user.test.js
@@ -3,17 +3,43 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 const mongoose = require("mongoose");
 
-const payload = {
-  _id: new mongoose.Types.ObjectId().toHexString(),
-  isAdmin: true
+const exampleUser = {
+  firstName: "firstName",
+  lastName: "lastName",
+  email: "exampleUser@address.com",
+  password: "password",
+  phone: "11111111",
 };
 
-const user = new User(payload);
+
 
 describe("user.generateAuthToken", () => {
-  it("should return a valid JWT", () => {
-    const token = user.generateAuthToken();
+
+  it("should return a valid JWT for admin user", async() => {
+    const admin = new User({...exampleUser, isAdmin: true});
+
+    const payload = {
+      _id: admin._id.toString(),
+      name: admin.firstName + " " + admin.lastName,
+      isAdmin: true
+    };
+    const token = admin.generateAuthToken();
     const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     expect(decoded).toMatchObject(payload);
   });
-});
+
+  it("should return a valid JWT for non admin user", () => {
+    const nonAdmin = new User({...exampleUser});
+
+    const payload = {
+      _id: nonAdmin._id.toString(),
+      name: nonAdmin.firstName + " " + nonAdmin.lastName,
+      isAdmin: false
+    };
+    const token = nonAdmin.generateAuthToken();
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+    expect(decoded).toMatchObject(payload);
+  });
+  })
+
+
