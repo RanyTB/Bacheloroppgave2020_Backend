100a63a19558889d1da11cf6392db6c9133ae398
rany-94 <ranytb@gmail.com>
2020-04-22 12:42:33 +0200

    remove corsoptions

--- startup/cors.js
+++ startup/cors.js
@@ -2,9 +2,9 @@ const cors = require("cors");
 
 const corsOptions = {
   origin: "http://localhost:3000",
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 200,
 };
 
-module.exports = function(app) {
-  app.use(cors(corsOptions));
+module.exports = function (app) {
+  app.use(cors());
 };
