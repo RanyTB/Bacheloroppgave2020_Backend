c2f27d6726dbf11888ca54149fb72ac423b82a64
Markus Hellestveit <markus.bikilla.hellestveit@gmail.com>
2020-04-27 12:34:28 +0200

    modify suggestionSchema

--- models/suggestion.js
+++ models/suggestion.js
@@ -8,17 +8,22 @@ const suggestionSchema = new mongoose.Schema({
     maxlength: 200,
     required: true,
   },
-  name: {
-    type: String,
-    minlength: 1,
-    maxlength: 510,
+  user: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        minlength: 2,
+        maxlength: 510,
+        required: true,
+      },
+    }),
     required: true,
   },
 });
 
 function validateSuggestion(suggestion) {
   const schema = Joi.object({
-    suggestion: Joi.string().required().min(1).max(200),
+    suggestion: Joi.string().required().min(6).max(3000),
     name: Joi.string().required().min(1).max(510),
   });
 
